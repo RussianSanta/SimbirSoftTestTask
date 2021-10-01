61134680b161811bebcae4129bb6eb6d23decc42
Максим <58334796+RussianSanta@users.noreply.github.com>
2021-10-01 15:54:04 +0300

    Rename SG_node_start.bat to bat_files/SG_node_start.bat

---
+++ bat_files/SG_node_start.bat
@@ -0,0 +1,1 @@
+java -Dwebdriver.chrome.driver=chromedriver_win32/chromedriver.exe -jar selenium-server-standalone-3.141.59.jar -role webdriver -hub http://localhost:4444/grid/register -port 5558 -browser browserName=chrome
